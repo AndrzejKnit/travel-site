d853e97ea3dbfb7fb4f79d2cce1af43f0b7a90e2
AndrzejKnit <andrzej.knitter@gmail.com>
2018-06-22 11:13:49 +0200

    lesson 43

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,21 +1,4 @@
-var $ = require('jquery');
-import Person from './modules/Person';
+import MobileMenu from './modules/MobileMenu';
 
-
-class Adult extends Person {
-    payTaxes() {
-        console.log(this.name + " now owes 0$ in taxes.");
-    }
-}
-
-alert("This test abc123");
-
-var john = new Person("John Doe", "blue");
-john.greet();
-
-var jane = new Adult("John Doe", "orange");
-jane.greet();
-jane.payTaxes();
-
-$("h1").remove();
+var mobileMenu = new MobileMenu();
 
